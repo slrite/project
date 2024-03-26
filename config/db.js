f5faf4cddc596c6bf08e8bd8e5afa6bd5b8b720b
slrite <164995041+slrite@users.noreply.github.com>
2024-03-26 14:30:58 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDB=async()=>{
     try{
-        await mongoose.connect("mongodb+srv://2140145:cCmh81F1k63bA3fK@cluster0.xvuetvz.mongodb.net/register");
+        await mongoose.connect(mongodb+srv://2140145:cCmh81F1k63bA3fK@cluster0.xvuetvz.mongodb.net/register);
     }catch(error)
     {
         console.log(`error in connection DB ${error}`);
